6e29e49367850db519e34fa1110cde91020e0cca
surfert17 <surfert17@gmail.com>
2026-02-04 18:38:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -40,4 +40,6 @@ app.get('/test', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
